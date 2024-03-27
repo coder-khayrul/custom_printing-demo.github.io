ea27db91f4f72264fd9210e4d032d37cde7d776b
khayrul200822 <khayrulislam200822@gmail.com>
2024-03-27 15:21:30 +0600

    updated preloader intro style

--- js/app.js
+++ js/app.js
@@ -4,43 +4,43 @@ let single_elements = document.querySelectorAll(".intro_element");
 let intro_preloader = document.querySelector(".intro_preloader");
 let preloader_close = document.querySelector(".intro_close");
 
+//preloader handler function
+
 
 preloader_close.addEventListener("click", () => {
     intro_preloader.classList.add("hide")
 })
 
-// Function to show single elements one by one with a delay
+// Function to show single elements one by one 
 function showElementsWithDelay() {
     let index = 0;
     const interval = setInterval(() => {
-        // Show current element
+   
         single_elements[index].style.display = "flex";
-        // Hide the previously shown element
+   
         if (index > 0) {
             single_elements[index - 1].style.display = "none";
         }
         index++;
-        // If all elements have been displayed, clear the interval
+      
         if (index >= single_elements.length) {
             clearInterval(interval);
         }
-    }, 1000); // 1 minute delay
+    }, 1000); 
 
-    // Show the last element after the loop ends
+
     setTimeout(() => {
         single_elements[single_elements.length - 1].style.display = "flex";
     }, single_elements.length * 1000);
 }
 
 
-// Call the function to start showing elements after 1 minute
 setTimeout(showElementsWithDelay, 0);
 const intro_preloader_handler = () => {
 
     intro_preloader.classList.add("show");
     setTimeout(() => {
         intro_preloader.classList.remove("show");
-    }, 8000);
+    }, 15000);
 }
-
-// window.addEventListener("load", intro_preloader_handler);
+window.addEventListener("load", intro_preloader_handler)
