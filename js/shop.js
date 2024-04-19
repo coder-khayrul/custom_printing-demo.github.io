e9711d615719410ec51ed86e2c9404b2f24fb389
khayrul200822 <khayrulislam200822@gmail.com>
2024-04-20 01:47:39 +0600

    back to top button on all pages

--- js/shop.js
+++ js/shop.js
@@ -787,6 +787,32 @@ price_ranges.forEach(input => {
 })
 
 
+//bottom to top button script
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#177AFE ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
+
+
+
+
+
 // Parallax footer area
 const parallax_footer = document.querySelector(".parallax_footer_area");
 const upper_area = document.querySelector(".extra_service");
